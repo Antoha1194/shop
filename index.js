a95cd4222245bb7213fe7488b53ca7e36dcb8b73
Legotin Anton <ant.legotin@yandex.ru>
2021-03-21 14:13:50 +0500

    Lesson2

--- index.js
+++ index.js
@@ -1,39 +1,104 @@
-const goods = [
-    {
-        img: '/img/shirt.jpg', 
-        title: 'Shirt', 
-        price: 150 
-    },
-    { 
-        img: '/img/socks.jpg',
-        title: 'Socks',
-        price: 50 
-    },
-    { 
-        img: '/img/jacket.jpg',
-        title: 'Jacket',
-        price: 350 
-    },
-    { 
-        img: '/img/shoes.jpg',
-        title: 'Shoes', 
-        price: 250 
+class GoodsItem{
+    constructor(id, title, price,img ){
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.img = img;
     }
-  ];
-  
-  const renderGoodsItem = (img ,title, price) => {
-    return `<div class="goods-item">
-                <img class="goods-item__img" src="${img}" alt="${title}">
-                <div class="goods-item__text">
-                    <h3 class="goods-item__title">${title}</h3>
-                    <p class="good-item__price">Цена: ${price} руб.</p>
-                </div>
-            </div>`;
-  };
-  
-  const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoodsItem(item.img ,item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList.join('');
-  }
-  
-  renderGoodsList(goods);
+    render(){
+        return `<div class="goods-item">
+                    <img class="goods-item__img" src="${this.img}" alt="${this.title}">
+                    <div class="goods-item__text">
+                        <h3 class="goods-item__title">${this.title}</h3>
+                        <p class="good-item__price">Цена: ${this.price} руб.</p>
+                    </div>
+                    <div class="add-basket">
+                        <button class="add-basket__button" data-id = ${this.id}>Добавить в корзину</button>
+                        <input class="add-basket__column" type="number" value="1">
+                    </div>
+                    
+                </div>`
+    }
+}
+
+class GoodsList{
+    constructor(){
+        this.goods = [];
+    }
+
+    fetchGoods(){
+        this.goods = [
+            { id: 1, title: 'Shirt', price: 150, img: "/img/shirt.jpg" },
+            { id: 2, title: 'Socks', price: 50, img: "/img/socks.jpg" },
+            { id: 3, title: 'Jacket', price: 350, img: "/img/jacket.jpg" },
+            { id: 4, title: 'Shoes', price: 250, img: "/img/shoes.jpg" },
+        ];
+    }
+
+    sumPrice(){
+        let sum = 0;
+        this.goods.map(item => sum += item.price);
+        return sum;
+    }
+
+    render() {
+        let listHtml = '';
+        this.goods.map(item => listHtml += new GoodsItem(item.id, item.title, item.price, item.img).render());
+        document.querySelector('.goods-list').innerHTML = listHtml;
+      }    
+}
+
+class Basket extends GoodsList{
+    
+
+    add(item){
+        this.goods.push(item);
+    }
+
+    clear(){
+        //TODO: Очистить корзину
+    }
+
+    payment(){
+        //TODO: Отправить корзину на оплату
+    }
+
+    render() {
+        //TODO:
+    }
+}
+
+class BasketItem extends GoodsItem{
+    constructor({id, title, price, img}, count){
+        super(id, title, price, img);
+        this.count = count;
+    }
+
+    drop(){
+        //TODO: Удалить элемент их корзины
+    }
+
+    render(){
+         //TODO:
+    }
+
+}
+
+function addItemInBasket(e){
+    const id = e.target.dataset.id;
+    const count = e.target.nextElementSibling.value;
+    list.goods.map(item => {
+        if(item.id == id)
+            basket.add(new BasketItem(item, count));
+    });
+    console.log(basket.goods);
+}
+
+
+
+const list = new GoodsList();
+const basket = new Basket();
+list.fetchGoods();
+list.render();
+console.log(list.sumPrice());
+document.querySelectorAll('.add-basket__button').forEach(item => item.addEventListener('click',addItemInBasket));
