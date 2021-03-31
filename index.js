8a9f3fe0305e57e79d027883ed2242990b1fd9ae
Legotin Anton <ant.legotin@yandex.ru>
2021-03-31 23:45:17 +0500

    lesson3

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+
+const  API = {
+    baseURL: "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses",
+    getCatalog: "/catalogData.json",
+    getBasket: "/getBasket.json",
+    addToBasket: "/addToBasket.json",
+    deleteFromBasket: "/deleteFromBasket.json"
+}
+
+
+
 class GoodsItem{
-    constructor(id, title, price,img ){
+    constructor(id, title, price,img = '/img/image.jpg' ){
         this.id = id;
         this.title = title;
         this.price = price;
@@ -26,37 +37,94 @@ class GoodsList{
         this.goods = [];
     }
 
-    fetchGoods(){
-        this.goods = [
-            { id: 1, title: 'Shirt', price: 150, img: "/img/shirt.jpg" },
-            { id: 2, title: 'Socks', price: 50, img: "/img/socks.jpg" },
-            { id: 3, title: 'Jacket', price: 350, img: "/img/jacket.jpg" },
-            { id: 4, title: 'Shoes', price: 250, img: "/img/shoes.jpg" },
-        ];
+    fetchGoods(url = '/', options = {}){
+        
+
+        return fetch(url, options).then(response => response.json());
     }
 
-    sumPrice(){
-        let sum = 0;
-        this.goods.map(item => sum += item.price);
-        return sum;
-    }
-
-    render() {
+    render(callback) {
         let listHtml = '';
-        this.goods.map(item => listHtml += new GoodsItem(item.id, item.title, item.price, item.img).render());
+        this.goods.forEach(item => {
+            let {id, title, price, img} = item;
+            listHtml += new GoodsItem(id, title, price, img).render();
+        })
         document.querySelector('.goods-list').innerHTML = listHtml;
+
+        document.querySelectorAll('.add-basket__button').forEach($item => {
+            
+            $item.onclick = (e) => {
+                const id_card = e.target.dataset.id;
+                const count = e.target.nextElementSibling.value;
+                list.goods.forEach(item => {
+                    let {id, title, price } = item;
+                    if(id == id_card)
+                        callback(new BasketItem(id, title, price, '/img/image.jpg', count));
+                        
+                });
+            }
+            
+        });
+
+        
       }    
 }
+
+
 
 class Basket extends GoodsList{
     
 
+    get sumPrice(){
+        let i = 0;
+        this.goods.forEach(item => {
+            console.log(item);    
+            i+= item.sumPrice
+        });
+        return i;
+    }
+
+    get coutProduct(){
+        let i = 0;
+        this.goods.forEach(item => {
+            console.log(item);    
+            i+= +item.count;
+        });
+        return i;
+    }
+
+    counter(){
+        let $count = document.querySelector(".count_product")
+
+        if(this.coutProduct > 0){
+            $count.innerHTML = this.coutProduct;
+            $count.style.display = "flex";
+        }else{
+            $count.style.display = "none";
+        }
+    }
+
+    drop(id){
+        this.goods = this.goods.filter(item => !(id == item.id));
+
+
+    }
+
     add(item){
         this.goods.push(item);
+        this.counter();
+        //API не использовал потому что CORS не пускает
+        // return fetch(API.baseURL + API.addToBasket, {
+        //     method: "POST",
+        //     headers:{
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify(item)
+        // }).then(response => response.json());
     }
 
     clear(){
-        //TODO: Очистить корзину
+        this.goods = [];
     }
 
     payment(){
@@ -64,41 +132,114 @@ class Basket extends GoodsList{
     }
 
     render() {
-        //TODO:
+        let listHtml = '';
+        let $basket = document.querySelector('.basket-list')
+        this.goods.forEach(item => {
+            let {id, title, price, img, count } = item;
+            listHtml += new BasketItem(id, title, price, img, count).render();
+        })
+
+        $basket.innerHTML = "";
+        $basket.innerHTML = listHtml + `<h2>Стоимость покупки: ${this.sumPrice} </h2>`;
+
+        document.querySelectorAll(".drop-basket__column").forEach($item => {
+            $item.oninput = (e) => {
+                const id_card = e.target.dataset.id;
+                const count = e.target.value;
+                this.goods.forEach(item => {
+                    if(item.id == id_card)
+                        item.count = count
+                });
+                this.render();
+                this.counter();
+            };
+            
+        })
+
+
+        document.querySelectorAll(".drop-basket__button").forEach($item => {
+            $item.onclick = (e) => {
+                const id = e.target.dataset.id;
+                this.drop(id);
+                this.counter();
+                this.render();
+            };
+        })
+
+        
+        
+        
     }
 }
 
 class BasketItem extends GoodsItem{
-    constructor({id, title, price, img}, count){
+    constructor(id, title, price, img, count){
         super(id, title, price, img);
         this.count = count;
     }
 
-    drop(){
-        //TODO: Удалить элемент их корзины
+    get sumPrice(){
+        return this.price * this.count;
     }
+        
 
     render(){
-         //TODO:
+        return `<div class="basket-item">
+                    <img class="basket-item__img" src="${this.img}" alt="${this.title}">
+                    <h3 class="basket-item__title">${this.title}</h3>
+                        
+                    <p class="basket-item__price">Цена: ${this.price} руб.</p>
+                    <p class="basket-item__price">Сумма: ${this.sumPrice} руб.</p>  
+                    <div class="drop-basket">
+                        
+                        <input class="drop-basket__column" type="number" data-id = ${this.id} value="${this.count}">
+                        <button class="drop-basket__button" data-id = ${this.id}>Удалить</button>
+                    </div>
+                    
+                </div>`
     }
 
-}
-
-function addItemInBasket(e){
-    const id = e.target.dataset.id;
-    const count = e.target.nextElementSibling.value;
-    list.goods.map(item => {
-        if(item.id == id)
-            basket.add(new BasketItem(item, count));
-    });
-    console.log(basket.goods);
 }
 
 
 
 const list = new GoodsList();
 const basket = new Basket();
-list.fetchGoods();
-list.render();
-console.log(list.sumPrice());
-document.querySelectorAll('.add-basket__button').forEach(item => item.addEventListener('click',addItemInBasket));
+
+list.fetchGoods(API.baseURL + API.getCatalog).then(data => {
+
+    data.forEach(item => {
+        let {id_product, product_name, price } = item;
+        list.goods.push(new GoodsItem(id_product, product_name, price));
+    });
+
+    list.render(basket.add.bind(basket));
+})
+
+
+basket.fetchGoods(API.baseURL + API.getBasket).then(data => {
+
+    data.contents.forEach(item => {
+        let {id_product, product_name, price } = item;
+        //basket.goods.push(new BasketItem(id_product, product_name, price));
+        console.log(basket.goods);
+        
+    })
+    
+    
+})
+
+document.querySelector("#basketBtn").addEventListener('click', basket.render.bind(basket));
+document.querySelector("#basketBtn").addEventListener('click', () => {
+    document.querySelector(".basket-list").classList.toggle('hidden')
+});
+
+
+
+
+
+
+
+
+
+
